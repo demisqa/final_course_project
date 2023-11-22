5296041b297a0b2d8402ea7ba88814c6134a5b7e
demisqa <beloplotovd@gmail.com>
2023-11-23 01:14:55 +0200

    update the autocompletion in tests

--- conftest.py
+++ conftest.py
@@ -16,7 +16,7 @@ def pytest_addoption(parser):
 
 
 @pytest.fixture(scope="function")
-def browser(request):
+def browser(request) -> webdriver:
     browser_name = request.config.getoption("browser_name")
     user_language = request.config.getoption("language")
     browser = None
--- test_main_page.py
+++ test_main_page.py
@@ -1,6 +1,7 @@
+from selenium.webdriver.remote.webdriver import WebDriver
 from selenium.webdriver.common.by import By
 
-def test_guest_can_go_to_login_page(browser):
+def test_guest_can_go_to_login_page(browser: WebDriver):
     link = "http://selenium1py.pythonanywhere.com/"
     browser.get(link)
     login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
