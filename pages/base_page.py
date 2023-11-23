8317372a2e2394575ca465689db989d93a13a0a7
demisqa <beloplotovd@gmail.com>
2023-11-23 20:03:58 +0200

    add BasePage and MainPage classes and update tests

---
+++ pages/base_page.py
@@ -0,0 +1,19 @@
+from selenium.webdriver.remote.webdriver import WebDriver
+from selenium.common.exceptions import NoSuchElementException
+
+class BasePage():
+    
+    def __init__(self, browser : WebDriver, url, timeout=10):
+        self.browser = browser
+        self.url = url
+        self.browser.implicitly_wait(timeout)
+
+    def open(self):
+        self.browser.get(self.url)
+
+    def is_element_present(self, by, value):
+        try:
+            self.browser.find_element(by, value)
+        except (NoSuchElementException):
+            return False
+        return True
