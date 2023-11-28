bcc6f4176943afda8051f03c3ddc3b1c5e9e475c
demisqa <beloplotovd@gmail.com>
2023-11-28 17:42:01 +0200

    add paremitrize fixture for product page for add to basket test

--- test_product_page.py
+++ test_product_page.py
@@ -1,9 +1,12 @@
+import pytest
 from pages.product_page import ProductPage
 from selenium.webdriver.remote.webdriver import WebDriver
 
-
-def test_guest_can_add_product_to_basket(browser : WebDriver):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
+@pytest.mark.parametrize('link',
+    [f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{i}"
+     if i!=7 else pytest.param(7, marks=pytest.mark.xfail(reason="current offer does not work"))
+     for i in range(10)])
+def test_guest_can_add_product_to_basket(browser : WebDriver, link):
     product_page = ProductPage(browser, link)
     product_page.open()
     product_page.add_product_to_basket()
