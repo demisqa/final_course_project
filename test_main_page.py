250ffe7b922a9246a2fd49eb1b9ed36ebc45b1ed
demisqa <beloplotovd@gmail.com>
2023-12-01 17:47:42 +0200

    update tests for product page and main page by splitting by classes

--- test_main_page.py
+++ test_main_page.py
@@ -1,42 +1,47 @@
+import pytest
 from pages.main_page import MainPage
 from pages.login_page import LoginPage
 from pages.basket_page import BasketPage
 from selenium.webdriver.remote.webdriver import WebDriver
 
 
-def test_guest_can_go_to_login_page_from_main_page(browser : WebDriver):
-    link = "http://selenium1py.pythonanywhere.com/"
-    main_page = MainPage(browser, link)
-    main_page.open()
-    main_page.go_to_login_page()
-    login_page = LoginPage(browser, browser.current_url)
-    login_page.should_be_login_page()
+@pytest.mark.login_guest
+class TestLoginFromMainPage():
+    def test_guest_can_go_to_login_page_from_main_page(self, browser : WebDriver):
+        link = "http://selenium1py.pythonanywhere.com/"
+        main_page = MainPage(browser, link)
+        main_page.open()
+        main_page.go_to_login_page()
+        login_page = LoginPage(browser, browser.current_url)
+        login_page.should_be_login_page()
+    
+    def test_guest_should_see_login_link_from_main_page(self, browser : WebDriver):
+        link = "http://selenium1py.pythonanywhere.com/"
+        main_page = MainPage(browser, link)
+        main_page.open()
+        main_page.should_be_login_link()
 
-def test_guest_should_see_login_link_from_main_page(browser : WebDriver):
-    link = "http://selenium1py.pythonanywhere.com/"
-    main_page = MainPage(browser, link)
-    main_page.open()
-    main_page.should_be_login_link()
-
-def test_guest_can_go_to_basket_page_from_main_page(browser : WebDriver):
-    link = "http://selenium1py.pythonanywhere.com/"
-    main_page = MainPage(browser, link)
-    main_page.open()
-    main_page.go_to_basket_page()
-    basket_page = BasketPage(browser, browser.current_url)
-    basket_page.should_be_basket_url()
-
-def test_guest_should_see_basket_link_on_main_page(browser : WebDriver):
-    link = "http://selenium1py.pythonanywhere.com/"
-    main_page = MainPage(browser, link)
-    main_page.open()
-    main_page.should_be_basket_link()
-
-def test_guest_cant_see_product_in_basket_opened_from_main_page(browser : WebDriver):
-    link = "http://selenium1py.pythonanywhere.com/"
-    main_page = MainPage(browser, link)
-    main_page.open()
-    main_page.go_to_basket_page()
-    basket_page = BasketPage(browser, browser.current_url)
-    basket_page.check_basket_is_empty()
-    basket_page.should_be_empty_basket_message()
+@pytest.mark.basket_guest
+class TestBasketFromMainPage():
+    def test_guest_can_go_to_basket_page_from_main_page(self, browser : WebDriver):
+        link = "http://selenium1py.pythonanywhere.com/"
+        main_page = MainPage(browser, link)
+        main_page.open()
+        main_page.go_to_basket_page()
+        basket_page = BasketPage(browser, browser.current_url)
+        basket_page.should_be_basket_url()
+    
+    def test_guest_should_see_basket_link_on_main_page(self, browser : WebDriver):
+        link = "http://selenium1py.pythonanywhere.com/"
+        main_page = MainPage(browser, link)
+        main_page.open()
+        main_page.should_be_basket_link()
+    
+    def test_guest_cant_see_product_in_basket_opened_from_main_page(self, browser : WebDriver):
+        link = "http://selenium1py.pythonanywhere.com/"
+        main_page = MainPage(browser, link)
+        main_page.open()
+        main_page.go_to_basket_page()
+        basket_page = BasketPage(browser, browser.current_url)
+        basket_page.check_basket_is_empty()
+        basket_page.should_be_empty_basket_message()
--- test_product_page.py
+++ test_product_page.py
@@ -5,75 +5,80 @@ from pages.basket_page import BasketPage
 from selenium.webdriver.remote.webdriver import WebDriver
 
 
-@pytest.mark.parametrize('link',
-    [f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{i}"
-     if i!=7 else pytest.param(7, marks=pytest.mark.xfail(reason="current offer does not work"))
-     for i in range(10)])
-def test_guest_can_add_product_to_basket(browser : WebDriver, link):
-    product_page = ProductPage(browser, link)
-    product_page.open()
-    product_page.add_product_to_basket()
-    product_page.solve_quiz_and_get_code()
-    product_page.compare_price_of_added_product_to_basket(product_page.get_price(),\
-                                                          product_page.get_price_in_basket())
-    product_page.compare_name_of_added_product_to_basket(product_page.get_product_name(),\
-                                                         product_page.get_product_name_in_basket())
+class TestProductPage():
+    @pytest.mark.parametrize('link',
+        [f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{i}"
+        if i!=7 else pytest.param(7, marks=pytest.mark.xfail(reason="current offer does not work"))
+        for i in range(10)])
+    def test_guest_can_add_product_to_basket(self, browser : WebDriver, link):
+        product_page = ProductPage(browser, link)
+        product_page.open()
+        product_page.add_product_to_basket()
+        product_page.solve_quiz_and_get_code()
+        product_page.compare_price_of_added_product_to_basket(product_page.get_price(),\
+                                                            product_page.get_price_in_basket())
+        product_page.compare_name_of_added_product_to_basket(product_page.get_product_name(),\
+                                                            product_page.get_product_name_in_basket())
 
-@pytest.mark.xfail(reason="failed according to task")
-def test_guest_cant_see_success_message_after_adding_product_to_basket(browser : WebDriver):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209"
-    product_page = ProductPage(browser, link)
-    product_page.open()
-    product_page.add_product_to_basket()
-    product_page.should_not_be_success_message()
+    @pytest.mark.xfail(reason="failed according to task")
+    def test_guest_cant_see_success_message_after_adding_product_to_basket(self, browser : WebDriver):
+        link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209"
+        product_page = ProductPage(browser, link)
+        product_page.open()
+        product_page.add_product_to_basket()
+        product_page.should_not_be_success_message()
 
-def test_guest_cant_see_success_message(browser : WebDriver):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209"
-    product_page = ProductPage(browser, link)
-    product_page.open()
-    product_page.should_not_be_success_message()
+    def test_guest_cant_see_success_message(self, browser : WebDriver):
+        link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209"
+        product_page = ProductPage(browser, link)
+        product_page.open()
+        product_page.should_not_be_success_message()
 
-@pytest.mark.xfail(reason="failed according to task")
-def test_message_disappeared_after_adding_product_to_basket(browser : WebDriver):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209"
-    product_page = ProductPage(browser, link)
-    product_page.open()
-    product_page.add_product_to_basket()
-    product_page.should_disappear_success_message()
+    @pytest.mark.xfail(reason="failed according to task")
+    def test_message_disappeared_after_adding_product_to_basket(self, browser : WebDriver):
+        link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209"
+        product_page = ProductPage(browser, link)
+        product_page.open()
+        product_page.add_product_to_basket()
+        product_page.should_disappear_success_message()
 
-def test_guest_should_see_login_link_on_product_page(browser : WebDriver):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/the-city-and-the-stars_95/"
-    product_page = ProductPage(browser, link)
-    product_page.open()
-    product_page.should_be_login_link()
+@pytest.mark.login_guest
+class TestLoginFromProductPage():
+    def test_guest_should_see_login_link_on_product_page(self, browser : WebDriver):
+        link = "http://selenium1py.pythonanywhere.com/catalogue/the-city-and-the-stars_95/"
+        product_page = ProductPage(browser, link)
+        product_page.open()
+        product_page.should_be_login_link()
+    
+    def test_guest_can_go_to_login_page_from_product_page(self, browser : WebDriver):
+        link = "http://selenium1py.pythonanywhere.com/catalogue/the-city-and-the-stars_95/"
+        product_page = ProductPage(browser, link)
+        product_page.open()
+        product_page.go_to_login_page()
+        login_page = LoginPage(browser, browser.current_url)
+        login_page.should_be_login_page()
 
-def test_guest_can_go_to_login_page_from_product_page(browser : WebDriver):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/the-city-and-the-stars_95/"
-    product_page = ProductPage(browser, link)
-    product_page.open()
-    product_page.go_to_login_page()
-    login_page = LoginPage(browser, browser.current_url)
-    login_page.should_be_login_page()
-
-def test_guest_can_go_to_basket_page_from_product_page(browser : WebDriver):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/the-city-and-the-stars_95/"
-    product_page = ProductPage(browser, link)
-    product_page.open()
-    product_page.go_to_basket_page()
-    basket_page = BasketPage(browser, browser.current_url)
-    basket_page.should_be_basket_url()
-
-def test_guest_should_see_basket_link_on_product_page(browser : WebDriver):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/the-city-and-the-stars_95/"
-    product_page = ProductPage(browser, link)
-    product_page.open()
-    product_page.should_be_basket_link()
-
-def test_guest_cant_see_product_in_basket_opened_from_product_page(browser : WebDriver):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/the-city-and-the-stars_95/"
-    product_page = ProductPage(browser, link)
-    product_page.open()
-    product_page.go_to_basket_page()
-    basket_page = BasketPage(browser, browser.current_url)
-    basket_page.check_basket_is_empty()
-    basket_page.should_be_empty_basket_message()
+@pytest.mark.basket_guest
+class TestBasketFromProductPage():
+    def test_guest_can_go_to_basket_page_from_product_page(self, browser : WebDriver):
+        link = "http://selenium1py.pythonanywhere.com/catalogue/the-city-and-the-stars_95/"
+        product_page = ProductPage(browser, link)
+        product_page.open()
+        product_page.go_to_basket_page()
+        basket_page = BasketPage(browser, browser.current_url)
+        basket_page.should_be_basket_url()
+    
+    def test_guest_should_see_basket_link_on_product_page(self, browser : WebDriver):
+        link = "http://selenium1py.pythonanywhere.com/catalogue/the-city-and-the-stars_95/"
+        product_page = ProductPage(browser, link)
+        product_page.open()
+        product_page.should_be_basket_link()
+    
+    def test_guest_cant_see_product_in_basket_opened_from_product_page(self, browser : WebDriver):
+        link = "http://selenium1py.pythonanywhere.com/catalogue/the-city-and-the-stars_95/"
+        product_page = ProductPage(browser, link)
+        product_page.open()
+        product_page.go_to_basket_page()
+        basket_page = BasketPage(browser, browser.current_url)
+        basket_page.check_basket_is_empty()
+        basket_page.should_be_empty_basket_message()
